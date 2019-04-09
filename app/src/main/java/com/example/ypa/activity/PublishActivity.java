f9ed1fa73bdfdf72a7247d1865c3acd1d048d215
sohuy <amfnd0416@naver.com>
2019-04-10 00:07:19 +0900

    수정본

---
+++ app/src/main/java/com/example/ypa/activity/PublishActivity.java
@@ -0,0 +1,174 @@
+package com.example.ypa.activity;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+import com.example.ypa.R;
+import com.example.ypa.Utils;
+import com.example.ypa.send.task;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.OnCheckedChanged;
+
+public class PublishActivity extends BaseActivity {
+    public static final String ARG_TAKEN_PHOTO_URI = "arg_taken_photo_uri";
+
+    @BindView(R.id.tbFollowers)
+    ToggleButton tbFollowers;
+    @BindView(R.id.tbDirect)
+    ToggleButton tbDirect;
+    @BindView(R.id.ivPhoto)
+    ImageView ivPhoto;
+
+    private boolean propagatingToggleState = false;
+    private Uri photoUri;
+    private int photoSize;
+
+    public static void openWithPhotoUri(Activity openingActivity, Uri photoUri) {
+        Intent intent = new Intent(openingActivity, PublishActivity.class);
+        intent.putExtra(ARG_TAKEN_PHOTO_URI, photoUri);
+        openingActivity.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_grey600_24dp);
+        photoSize = getResources().getDimensionPixelSize(R.dimen.publish_photo_thumbnail_size);
+
+        if (savedInstanceState == null) {
+            photoUri = getIntent().getParcelableExtra(ARG_TAKEN_PHOTO_URI);
+        } else {
+            photoUri = savedInstanceState.getParcelable(ARG_TAKEN_PHOTO_URI);
+        }
+        updateStatusBarColor();
+
+        ivPhoto.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                ivPhoto.getViewTreeObserver().removeOnPreDrawListener(this);
+                loadThumbnailPhoto();
+                return true;
+            }
+        });
+
+        //0330
+        Button send = (Button)findViewById(R.id.btnSend);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    String sendmsg = "vision_write";
+                    EditText pEdit;
+                    String gEdit = "";
+                    pEdit = (EditText)findViewById(R.id.etDescription);
+                    gEdit = pEdit.getText().toString();
+
+                    try{
+                        String rst = new task(sendmsg).execute(gEdit,"vision_write").get();
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+
+                }catch (Exception e) {
+                }
+            }
+        });
+        //end 0330
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void updateStatusBarColor() {
+        if (Utils.isAndroid5()) {
+            getWindow().setStatusBarColor(0xff888888);
+        }
+    }
+
+    private void loadThumbnailPhoto() {
+        ivPhoto.setScaleX(0);
+        ivPhoto.setScaleY(0);
+        Picasso.with(this)
+                .load(photoUri)
+                .centerCrop()
+                .resize(photoSize, photoSize)
+                .into(ivPhoto, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        ivPhoto.animate()
+                                .scaleX(1.f).scaleY(1.f)
+                                .setInterpolator(new OvershootInterpolator())
+                                .setDuration(400)
+                                .setStartDelay(200)
+                                .start();
+                    }
+
+                    @Override
+                    public void onError() {
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_publish, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_publish) {
+            bringMainActivityToTop();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void bringMainActivityToTop() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        intent.setAction(MainActivity.ACTION_SHOW_LOADING_ITEM);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(ARG_TAKEN_PHOTO_URI, photoUri);
+    }
+
+    @OnCheckedChanged(R.id.tbFollowers)
+    public void onFollowersCheckedChange(boolean checked) {
+        if (!propagatingToggleState) {
+            propagatingToggleState = true;
+            tbDirect.setChecked(!checked);
+            propagatingToggleState = false;
+        }
+    }
+
+    @OnCheckedChanged(R.id.tbDirect)
+    public void onDirectCheckedChange(boolean checked) {
+        if (!propagatingToggleState) {
+            propagatingToggleState = true;
+            tbFollowers.setChecked(!checked);
+            propagatingToggleState = false;
+        }
+    }
+}
